1b26890dab9f2889f883c17b1101e55b8099729c
Pavel Kirilin <win10@list.ru>
2026-03-23 21:48:54 +0100

    Added JS information parsed from message. (#16)

--- src/js/message.rs
+++ src/js/message.rs
@@ -1,18 +1,53 @@
 use pyo3::{
     Bound, Py, PyAny, Python,
-    types::{PyBytes, PyDict},
+    types::{PyBytes, PyDateTime, PyDict},
 };
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
 use crate::{
     exceptions::rust_err::{NatsrpyError, NatsrpyResult},
-    utils::{natsrpy_future, py_types::TimeValue},
+    utils::{
+        natsrpy_future,
+        py_types::{TimeValue, ToPyDate},
+    },
 };
+
+#[derive(Debug, Clone)]
+pub struct JSInfo {
+    pub domain: Option<String>,
+    pub acc_hash: Option<String>,
+    pub stream: String,
+    pub consumer: String,
+    pub stream_sequence: u64,
+    pub consumer_sequence: u64,
+    pub delivered: i64,
+    pub pending: u64,
+    pub published: time::OffsetDateTime,
+    pub token: Option<String>,
+}
+
+impl From<async_nats::jetstream::message::Info<'_>> for JSInfo {
+    fn from(value: async_nats::jetstream::message::Info) -> Self {
+        Self {
+            domain: value.domain.map(ToString::to_string),
+            acc_hash: value.acc_hash.map(ToString::to_string),
+            stream: value.stream.to_string(),
+            consumer: value.consumer.to_string(),
+            stream_sequence: value.stream_sequence,
+            consumer_sequence: value.consumer_sequence,
+            delivered: value.delivered,
+            pending: value.pending,
+            published: value.published,
+            token: value.token.map(ToString::to_string),
+        }
+    }
+}
 
 #[pyo3::pyclass]
 pub struct JetStreamMessage {
     message: crate::message::Message,
+    info: JSInfo,
     acker: Arc<RwLock<async_nats::jetstream::message::Acker>>,
 }
 
@@ -20,9 +55,11 @@ impl TryFrom<async_nats::jetstream::Message> for JetStreamMessage {
     type Error = NatsrpyError;
 
     fn try_from(value: async_nats::jetstream::Message) -> Result<Self, Self::Error> {
+        let js_info = JSInfo::from(value.info()?);
         let (message, acker) = value.split();
         Ok(Self {
             message: message.try_into()?,
+            info: js_info,
             acker: Arc::new(RwLock::new(acker)),
         })
     }
@@ -67,6 +104,57 @@ impl JetStreamMessage {
     #[getter]
     pub const fn headers(&mut self) -> &Py<PyDict> {
         &self.message.headers
+    }
+
+    #[getter]
+    pub const fn domain(&mut self) -> &Option<String> {
+        &self.info.domain
+    }
+
+    #[getter]
+    #[must_use]
+    pub const fn acc_hash(&self) -> &Option<String> {
+        &self.info.acc_hash
+    }
+    #[getter]
+    #[must_use]
+    pub const fn stream(&self) -> &str {
+        self.info.stream.as_str()
+    }
+    #[getter]
+    #[must_use]
+    pub const fn consumer(&self) -> &str {
+        self.info.consumer.as_str()
+    }
+    #[getter]
+    #[must_use]
+    pub const fn stream_sequence(&self) -> u64 {
+        self.info.stream_sequence
+    }
+    #[getter]
+    #[must_use]
+    pub const fn consumer_sequence(&self) -> u64 {
+        self.info.consumer_sequence
+    }
+    #[getter]
+    #[must_use]
+    pub const fn delivered(&self) -> i64 {
+        self.info.delivered
+    }
+    #[getter]
+    #[must_use]
+    pub const fn pending(&self) -> u64 {
+        self.info.pending
+    }
+    #[getter]
+    pub fn published<'py>(&self, py: Python<'py>) -> NatsrpyResult<Bound<'py, PyDateTime>> {
+        Ok(self.info.published.to_py_date(py)?)
+    }
+
+    #[getter]
+    #[must_use]
+    pub const fn token(&self) -> &Option<String> {
+        &self.info.token
     }
 
     #[pyo3(signature=(double=false))]
