e7d898824b79fa70efcda0294eb2880e2a0bd07e
Pavel Kirilin <win10@list.ru>
2026-03-25 10:43:17 +0100

    Added stubtest in pre-commit config.

---
+++ scripts/stubtest.py
@@ -0,0 +1,17 @@
+# ruff: noqa
+import os
+import subprocess
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+
+
+def main():
+    print(os.environ["VIRTUAL_ENV"])
+    subprocess.run(["pip", "install", "mypy"], check=True)
+    subprocess.run(["maturin", "dev", "--uv"], cwd=ROOT_DIR, check=True)
+    os.execvpe("stubtest", ["--ignore-disjoint-bases", "natsrpy"], env=os.environ)
+
+
+if __name__ == "__main__":
+    main()
