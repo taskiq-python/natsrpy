464f392bf2d25e82d584ebac35797a033be80c19
Pavel Kirilin <win10@list.ru>
2026-03-25 02:35:47 +0100

    Added module in __all__.

--- python/natsrpy/__init__.py
+++ python/natsrpy/__init__.py
@@ -1,8 +1,4 @@
+from . import js
 from ._natsrpy_rs import CallbackSubscription, IteratorSubscription, Message, Nats
 
-__all__ = [
-    "CallbackSubscription",
-    "IteratorSubscription",
-    "Message",
-    "Nats",
-]
+__all__ = ["CallbackSubscription", "IteratorSubscription", "Message", "Nats", "js"]
