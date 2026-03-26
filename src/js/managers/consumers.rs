c86ca998f38474fe976a29fed2f965bfef8464d4
Pavel Kirilin <win10@list.ru>
2026-03-26 17:42:49 +0100

    Added missing API methods. (#33)

--- src/js/managers/consumers.rs
+++ src/js/managers/consumers.rs
@@ -1,13 +1,143 @@
 use std::{sync::Arc, time::Duration};
 
-use pyo3::{Bound, FromPyObject, IntoPyObjectExt, PyAny, Python};
-use tokio::sync::RwLock;
+use futures_util::StreamExt;
+use pyo3::{Bound, FromPyObject, IntoPyObjectExt, PyAny, PyRef, Python};
+use tokio::sync::{Mutex, RwLock};
 
 use crate::{
     exceptions::rust_err::{NatsrpyError, NatsrpyResult},
     js::consumers::{self, pull::PullConsumer, push::PushConsumer},
-    utils::{natsrpy_future, py_types::TimeValue},
+    utils::{
+        futures::natsrpy_future_with_timeout, natsrpy_future, py_types::TimeValue,
+        streamer::Streamer,
+    },
 };
+
+#[pyo3::pyclass]
+pub struct ConsumersIterator {
+    streamer: Arc<
+        Mutex<
+            Streamer<
+                Result<
+                    async_nats::jetstream::consumer::Info,
+                    async_nats::jetstream::stream::ConsumersError,
+                >,
+            >,
+        >,
+    >,
+    stream: Arc<RwLock<async_nats::jetstream::stream::Stream<async_nats::jetstream::stream::Info>>>,
+}
+
+#[pyo3::pyclass]
+pub struct ConsumersNamesIterator {
+    streamer: Arc<Mutex<Streamer<Result<String, async_nats::jetstream::stream::ConsumersError>>>>,
+}
+
+impl ConsumersNamesIterator {
+    #[must_use]
+    pub fn new(
+        streamer: Streamer<Result<String, async_nats::jetstream::stream::ConsumersError>>,
+    ) -> Self {
+        Self {
+            streamer: Arc::new(Mutex::new(streamer)),
+        }
+    }
+}
+
+#[pyo3::pymethods]
+impl ConsumersNamesIterator {
+    #[must_use]
+    pub const fn __aiter__(slf: PyRef<Self>) -> PyRef<Self> {
+        slf
+    }
+
+    #[pyo3(signature=(timeout=None))]
+    pub fn next<'py>(
+        &self,
+        py: Python<'py>,
+        timeout: Option<TimeValue>,
+    ) -> NatsrpyResult<Bound<'py, PyAny>> {
+        let ctx = self.streamer.clone();
+        natsrpy_future_with_timeout(py, timeout, async move {
+            let value = ctx.lock().await.next().await;
+            match value {
+                Some(name) => Ok(name?),
+                None => Err(NatsrpyError::AsyncStopIteration),
+            }
+        })
+    }
+
+    pub fn __anext__<'py>(&self, py: Python<'py>) -> NatsrpyResult<Bound<'py, PyAny>> {
+        self.next(py, None)
+    }
+}
+
+impl ConsumersIterator {
+    pub fn new(
+        stream: Arc<
+            RwLock<async_nats::jetstream::stream::Stream<async_nats::jetstream::stream::Info>>,
+        >,
+        streamer: Streamer<
+            Result<
+                async_nats::jetstream::consumer::Info,
+                async_nats::jetstream::stream::ConsumersError,
+            >,
+        >,
+    ) -> Self {
+        Self {
+            stream,
+            streamer: Arc::new(Mutex::new(streamer)),
+        }
+    }
+}
+
+#[pyo3::pymethods]
+impl ConsumersIterator {
+    #[must_use]
+    pub const fn __aiter__(slf: PyRef<Self>) -> PyRef<Self> {
+        slf
+    }
+
+    #[pyo3(signature=(timeout=None))]
+    pub fn next<'py>(
+        &self,
+        py: Python<'py>,
+        timeout: Option<TimeValue>,
+    ) -> NatsrpyResult<Bound<'py, PyAny>> {
+        let ctx = self.streamer.clone();
+        let stream = self.stream.clone();
+        natsrpy_future_with_timeout(py, timeout, async move {
+            let value = ctx.lock().await.next().await;
+            match value {
+                Some(info) => {
+                    let info = info?;
+                    let Some(consumer_name) = info.config.name else {
+                        return Err(NatsrpyError::SessionError(String::from(
+                            "Received consumer without a name.",
+                        )));
+                    };
+                    // That means that the consumer is PushBased.
+                    if info.config.deliver_subject.is_some() {
+                        let consumer = consumers::push::consumer::PushConsumer::new(
+                            stream.read().await.get_consumer(&consumer_name).await?,
+                        );
+                        Ok(Python::attach(|py| consumer.into_py_any(py))?)
+                    } else {
+                        let consumer = consumers::pull::consumer::PullConsumer::new(
+                            stream.read().await.get_consumer(&consumer_name).await?,
+                        );
+                        Ok(Python::attach(|py| consumer.into_py_any(py))?)
+                    }
+                }
+                None => Err(NatsrpyError::AsyncStopIteration),
+            }
+        })
+    }
+
+    pub fn __anext__<'py>(&self, py: Python<'py>) -> NatsrpyResult<Bound<'py, PyAny>> {
+        self.next(py, None)
+    }
+}
 
 #[pyo3::pyclass]
 pub struct ConsumersManager {
@@ -144,6 +274,25 @@ impl ConsumersManager {
         let ctx = self.stream.clone();
         natsrpy_future(py, async move {
             Ok(ctx.read().await.delete_consumer(&name).await?.success)
+        })
+    }
+
+    pub fn list<'py>(&self, py: Python<'py>) -> NatsrpyResult<Bound<'py, PyAny>> {
+        let ctx = self.stream.clone();
+        natsrpy_future(py, async move {
+            let consumers = ctx.read().await.consumers();
+            Ok(ConsumersIterator::new(
+                ctx.clone(),
+                Streamer::new(consumers),
+            ))
+        })
+    }
+
+    pub fn list_names<'py>(&self, py: Python<'py>) -> NatsrpyResult<Bound<'py, PyAny>> {
+        let ctx = self.stream.clone();
+        natsrpy_future(py, async move {
+            let consumers = ctx.read().await.consumer_names();
+            Ok(ConsumersNamesIterator::new(Streamer::new(consumers)))
         })
     }
 }
