0d5a528140930b50d0c5d4b652a5b0d12bbac10d
Pavel Kirilin <win10@list.ru>
2026-03-28 16:02:19 +0100

    Small enhancements.

--- src/utils/headers.rs
+++ src/utils/headers.rs
@@ -14,14 +14,14 @@ impl NatsrpyHeadermapExt for async_nats::HeaderMap {
     fn from_pydict(pydict: Bound<PyDict>) -> NatsrpyResult<Self> {
         let mut headermap = Self::new();
         for (name, val) in pydict {
-            let rs_name = name.extract::<String>()?;
-            if let Ok(parsed_str) = val.extract::<String>() {
+            let rs_name = name.extract::<&str>()?;
+            if let Ok(parsed_str) = val.extract::<&str>() {
                 headermap.insert(rs_name, parsed_str);
                 continue;
             }
             if let Ok(parsed_list) = val.extract::<Vec<String>>() {
                 for inner in parsed_list {
-                    headermap.append(rs_name.as_str(), inner);
+                    headermap.append(rs_name, inner);
                 }
                 continue;
             }
