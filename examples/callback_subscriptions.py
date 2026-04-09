f91797955f2d663af7cb237303390fab4e0480e4
Pavel Kirilin <win10@list.ru>
2026-04-09 20:55:48 +0200

    Updated callback example.

--- examples/callback_subscriptions.py
+++ examples/callback_subscriptions.py
@@ -11,6 +11,14 @@ async def main() -> None:
     async def callback(message: Message) -> None:
         print(f"[FROM_CALLBACK] {message.payload!r}")  # noqa: T201
 
+    async with nats.subscribe("cb-subj", callback=callback) as sub:
+        # Lets you unsubscribe after `n` messages.
+        await sub.unsubscribe(limit=1)
+        await nats.publish("cb-subj", "context-based")
+        # Waits for subscription to be drained.
+        # Reach limit in our case.
+        await sub.wait()
+
     # For callback subscriptions you can use detatch method.
     #
     # This method does the same as __enter__, however since
@@ -23,7 +31,7 @@ async def main() -> None:
     cb_sub = await nats.subscribe("cb-subj", callback=callback).detatch()
     await cb_sub.unsubscribe(limit=1)
 
-    nats.publish("cb-subj", "message for callback")
+    nats.publish("cb-subj", "detached version")
 
     # Waiting for subscriber to read all the messages.
     await cb_sub.wait()
